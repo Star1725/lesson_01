72b34b3d7172f9440159fcebdccd0a3c49492640
Sergey <starovoytov1725@gmail.com>
2020-06-25 16:03:22 +0300

    fixed some bug and redesigned user dialog for the task1

--- src/main/java/ru/geekbrains/lesson_03/guessTheNumber.java
+++ src/main/java/ru/geekbrains/lesson_03/guessTheNumber.java
@@ -8,41 +8,44 @@ import java.io.InputStreamReader;
  * Created by User on 024 24.06.20.
  */
 public class guessTheNumber {
-    private static int generatedNumber;
-    private static int numberOfUser;
     private static int countTry;
     private static boolean check;
-    private static boolean replay = true;
 
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException{
-        System.out.print("Здравствуй User. ");
+        System.out.print("Здравствуй User. ");//Приветствие. выбодится только при запуске игры
+        boolean replay;//переменная для повторения игры
         do {
-            check = false;
-            countTry = 3;
-            generatedNumber = numberGenerator();
-            System.out.println("Я загадал число от 0 до 9.\nПопрорбуй угадай. У тебя три попытки");
-            while (countTry > 0){
-                numberOfUser = readerNumberOfUser();
-                System.out.println(comparMethod(generatedNumber, numberOfUser));
-                if (check) break;
-            }
-            if (!check) System.out.println("Ты проиграл.");
-            System.out.println("Повторить игру ещё раз? 1 - да / 0 - нет");
-            while (true){
-                String s = reader.readLine();
-                if (s.equals("1")) {
-                    replay = true; break;
-                }
-                else if (s.equals("0")) {
-                    replay = false; break;
-                }
-                else {
-                    System.out.println("Тебе нужно ввести либо 1, либо 0");
-                }
+            //начальные условия
+            countTry = 3;//количество попыток = 3
+            check = false;//мы пока не угадали
+            int generatedNumber = numberGenerator(); //программа загадала число
+            System.out.println("Я загадал число от 0 до 9.\nПопробуй угадай. У тебя есть три попытки");
+
+            while (countTry >= 1){//контролируем количество попыток
+                int numberOfUser = readerNumberOfUser();//считываем число пользователя
+                countTry--;//отнимаем одну попытку
+                System.out.println(compareMethod(generatedNumber, numberOfUser));//выводим результат котроля попыток и сравнения чисел
+                if (check) break;//если числа совпали, User выиграл
             }
 
+            System.out.println("Повторить игру ещё раз? 1 - да / 0 - нет");
+            label:
+            while (true) {
+                String s = reader.readLine();
+                switch (s) {
+                    case "1":
+                        replay = true;
+                        break label;
+                    case "0":
+                        replay = false;
+                        break label;
+                    default:
+                        System.out.println("Тебе нужно ввести либо 1, либо 0");
+                        break;
+                }
+            }
         } while (replay);
     }
 
@@ -70,16 +73,16 @@ public class guessTheNumber {
     }
 
     //метод сравнения чисел
-    public static String comparMethod(int genNum, int userNum){
-        String str;
-        if (genNum == userNum) {
+    public static String compareMethod(int genNum, int userNum){
+        //
+        if (userNum < 0 || userNum > 9) return "Я же сказал от 0 до 9!  " + (countTry == 0 ? "Попытки кончились." : "Минус одна попытка.");
+        else if (genNum == userNum){
             check = true;
-            return str = "Ты угадал.";
+            return "Ты угадал.";
         }
+        else if(countTry == 0)return "Попытки кончились. GAME OVER!!! Я загадал - " + genNum;
         else{
-            check = false;
-            countTry--;
-            return str = genNum > userNum ? "Твоё число меньше. Можешь попробывать ещё " + countTry + " раз(а)"
+            return genNum > userNum ? "Твоё число меньше. Можешь попробывать ещё " + countTry + " раз(а)"
                     : "Твоё число больше. Можешь попробывать ещё " + countTry + " раз(а)";
         }
     }
